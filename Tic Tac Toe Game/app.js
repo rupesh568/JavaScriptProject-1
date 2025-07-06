179de48ba20f4de3aec7fcb808efac015fe21056
Rupesh568 <rupeshjaiswal999939@gmail.com>
2025-07-06 11:13:47 +0530

    Completed Tic Tac Toe Game

--- Tic Tac Toe Game/app.js
+++ Tic Tac Toe Game/app.js
@@ -1,5 +1,10 @@
 let boxes=document.querySelectorAll(".box");
-let button=document.querySelector(".resetbtn");
+let resetbutton=document.querySelector(".resetbtn");
+let newgamebtn=document.querySelector(".newgamebtn");
+let messageContainer=document.querySelector(".msg-container");
+let accessMsg=document.querySelector("#msg")
+
+
 
 
 let turnX=true;
@@ -10,13 +15,14 @@ const winPatterns=[
     [0,3,6],
     [1,4,7],
     [2,5,8],
-    [0,4,7],
+    [0,4,8],
     [2,4,6],
 ];
 
-
+let count=0;
 boxes.forEach((val)=>{
     val.addEventListener("click",(e)=>{
+    
        if(turnX){
         val.innerText="X";
         turnX=false;
@@ -25,6 +31,65 @@ boxes.forEach((val)=>{
         val.innerText="O";
         turnX=true;
        }
-
+       val.disabled=true;
+       checkWinner();
+       count++;
     })
 })
+
+const resetGame=()=>{
+    turnX=true;
+    boxenable();
+    messageContainer.classList.add("hide");
+
+
+}
+
+const boxenable=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+
+
+    }
+}
+
+const stop=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+
+}
+const checkWinner=()=>{
+    for(let patterns of winPatterns){
+        // console.log(patterns[0],patterns[1],patterns[2]);
+        // console.log(boxes[patterns[0]].innerText,boxes[patterns[1]].innerText,boxes[patterns[2]].innerText);
+        valpos1=boxes[patterns[0]].innerText;
+        valpos2=boxes[patterns[1]].innerText;
+        valpos3=boxes[patterns[2]].innerText;
+
+        if(valpos1!="" && valpos2!="" && valpos3!=""){
+            if(valpos1===valpos2 && valpos2===valpos3){
+                let win=`Congratulations ,Winner is ${valpos1}`;
+                messageContainer.classList.remove("hide");
+                accessMsg.innerText=win;
+                stop();
+            
+                
+
+                
+            }
+            else{
+                count=9;
+                accessMsg.innerText="Draw";
+            } 
+        }
+    }
+}
+ 
+let accessH1=document.querySelector("h1");
+accessH1.classList.add("h1");
+
+
+newgamebtn.addEventListener("click",resetGame);
+resetbutton.addEventListener("click",resetGame);
